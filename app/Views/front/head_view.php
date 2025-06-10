9cd9954dedb7054429187186ff8824038528ddd2
augusto <augusto@proyecto3.com>
2025-06-10 11:22:16 -0300

    Se corrige error que no permitia levantar la web

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php echo($titulo);?></title>
+    <title><?php echo($titulo);?></title>
 
     <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/style.css');?>" rel="stylesheet">
